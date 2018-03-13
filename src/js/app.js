b0fe108a1678eff4aadbae58f710739fcc053d2a
Helga Helen <oliatrubina@yandex.ru>
2018-03-13 15:47:27 +0300

    all pages

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,49 @@ $(document).ready(function () {
   
   'use strict'
   
+  // menu blackout
+  $(window).scroll(function () {
+    $('header').each(function () {
+      var newScroll = $(window).scrollTop();
+      var tempScroll = $(this).height() - newScroll;
+      if (tempScroll < 0) tempScroll = 0;
+      var tempOpacityBg = ((newScroll) / ($(window).height() / 3));
+      $('.header_overlay').css('opacity', tempOpacityBg);
+      var currentOpacityOverlay = ($('.header_overlay').css('opacity'));
+      if (currentOpacityOverlay >= 0.95) {
+        $('.header_overlay').css('opacity', '.95');
+      }
+    });
+  });
+  
+  // sticky logo on index page
+  
+  if($('.main_logo').length){
+    $(function () {
+      var box = $('.main_logo');
+      var top = box.offset().top - 14;
+      $(window).scroll(function () {
+        var windowpos = $(window).scrollTop();
+        if (windowpos < top) {
+          box.removeClass('sticky');
+  //        box.css({
+  //          'position': 'static',
+  //          'transform': 'none'
+  //        });
+        }
+        else {
+          box.addClass('sticky');
+  //        box.css({
+  //          'position': 'fixed',
+  //          'top': '14px',
+  //          'left': '50%',
+  //          'transform': 'translateX(-50%)'
+  //        });
+        }
+      });
+    }); 
+  }
+  
   // menu catalog
 //  $('.js-toggle_menu + span').on('click', function(){
 //    $(this).prev().addClass('is-open').next().next().slideDown(300).parent('li').siblings().find('.is-open').removeClass('is-open').next().next().slideUp(300);
@@ -16,69 +59,52 @@ $(document).ready(function () {
     autoplaySpeed: 4000    
   });
   
-//  $('.doors_slider').slick({
-//    arrows: true,
-//    infinite: false,
-//    slidesToShow: 3,
-//    slidesToScroll: 3,
-//    responsive: [
-//    {
-//      breakpoint: 1281,
-//      settings: {
-//        slidesToShow: 2,
-//        slidesToScroll: 2,
-//      }
-//    },
-//    {
-//      breakpoint: 993,
-//      settings: {
-//        slidesToShow: 3,
-//        slidesToScroll: 3,
-//      }
-//    },
-//    {
-//      breakpoint: 768,
-//      settings: {
-//        slidesToShow: 2,
-//        slidesToScroll: 2,
-//      }
-//    },
-//    {
-//      breakpoint: 481,
-//      settings: {
-//        slidesToShow: 1,
-//        slidesToScroll: 1,
-//        variableWidth: true
-//      }
-//    }
-//  ]
-//  });
-  
-//  $('.product_slider').slick({
-//    arrows: true    
-//  });
+  $('.js-slider').slick({
+    arrows: true,
+    dots: true,
+    infinite: true,
+    centerMode: true,
+//    autoplay: true,  
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
   
   // tabs
-//  $('.tabs_block').each(function () {
-//    $(this).find('.tab').each(function (i) {
-//      $(this).on('click', function (e) {
-//        e.preventDefault();
-//        $(this).addClass('active').siblings().removeClass('active').closest('.tabs_block').find('.tabs_content').removeClass('active').eq(i).addClass('active');
-//      });
-//    });
-//  });
+  $('.tabs_block').each(function () {
+    $(this).find('.tab').each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $(this).addClass('active').siblings().removeClass('active').closest('.tabs_block').find('.tabs_content').removeClass('active').eq(i).addClass('active');
+      });
+    });
+  });
   
-  // inputmask
-//  $('#call_phone, #popup_phone').mask('(99) 999-99-99');
-  
-  // select
-//  
-//  $('.js-select').select2({
-//    minimumResultsForSearch: Infinity 
-//  });
+//   inputmask
+  $('#w_phone, #p_phone').mask('+375 (99) 999-99-99');
   
   // popup
-  
-
+  	$('.js-popup').click(function (e) {
+  	  e.preventDefault();
+  	  var DataId = $(this).attr('data-id');
+  	  if (typeof DataId == "string") {
+  	    $('#' + DataId).fadeIn(300).css('display', 'flex');
+  	    $('body').css('overflow', 'hidden');
+  	  }
+  	});
+  	$('.popup').click(function (e) {
+  	  e = event || window.event
+  	  if (e.target == this) {
+  	    $(this).fadeOut(200);
+  	    $('body').css('overflow', 'auto');
+  	  }
+  	});
+  	$('.popup .link').click(function (e) {
+  	  e.preventDefault();
+  	  $(this).parents('.popup').fadeOut(200);
+  	  $('body').css('overflow', 'auto');
+  	});
+  	$('.popup .popup_content').click(function (e) {
+  	  e.stopPropagation();
+  	});
 
 });
