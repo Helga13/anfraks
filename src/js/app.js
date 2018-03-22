81f113844d67e1a457b5c846dc17535f363a1a0c
Helga Helen <oliatrubina@yandex.ru>
2018-03-22 13:07:38 +0300

    media

--- src/js/app.js
+++ src/js/app.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
   if($('.main_logo').length){
     $(function () {
       var box = $('.main_logo');
-      var top = box.offset().top - 14;
+      var top = box.offset().top + 14;
       $(window).scroll(function () {
         var windowpos = $(window).scrollTop();
         if (windowpos < top) {
@@ -45,11 +45,6 @@ $(document).ready(function () {
     }); 
   }
   
-  // menu catalog
-//  $('.js-toggle_menu + span').on('click', function(){
-//    $(this).prev().addClass('is-open').next().next().slideDown(300).parent('li').siblings().find('.is-open').removeClass('is-open').next().next().slideUp(300);
-//  });
-  
   // sliders
   $('.main_slider').slick({
     arrows: false,
@@ -64,9 +59,23 @@ $(document).ready(function () {
     dots: true,
     infinite: true,
     centerMode: true,
-//    autoplay: true,  
+//    autoplay: true,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+    {
+      breakpoint: 1401,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 993,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
   });
   
   // tabs
@@ -106,5 +115,53 @@ $(document).ready(function () {
   	$('.popup .popup_content').click(function (e) {
   	  e.stopPropagation();
   	});
+  
+  // murger menu
+  
+  $('.js-menu').on('click', function(){
+    $(this).next('.menu_block').addClass('is-visible').fadeIn(300);
+    $('.logo_container').css({'z-index': '0'});
+    $('body').css('overflow', 'hidden');
+  });
+  $('.js-close').on('click', function(){
+    $(this).parent('.menu_block').removeClass('is-visible').fadeOut(100);
+    $('.logo_container').css({'z-index': ''});
+    $('body').css('overflow', 'auto');
+  });
 
 });
+
+//
+//(function(){  // анонимная функция (function(){ })(), чтобы переменные "a" и "b" не стали глобальными
+//var a = document.querySelector('.main_logo'), b = null;  // селектор блока, который нужно закрепить
+//window.addEventListener('scroll', Ascroll, false);
+//document.body.addEventListener('scroll', Ascroll, false);  // если у html и body высота равна 100%
+//function Ascroll() {
+//  if (b == null) {  // добавить потомка-обёртку, чтобы убрать зависимость с соседями
+//    var Sa = getComputedStyle(a, ''), s = '';
+//    for (var i = 0; i < Sa.length; i++) {  // перечислить стили CSS, которые нужно скопировать с родителя
+//      if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+//        s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+//      }
+//    }
+//    b = document.createElement('div');  // создать потомка
+//    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+//    a.insertBefore(b, a.firstChild);  // поместить потомка в цепляющийся блок первым
+//    var l = a.childNodes.length;
+//    for (var i = 1; i < l; i++) {  // переместить во вновь созданного потомка всех остальных потомков (итого: создан потомок-обёртка, внутри которого по прежнему работают скрипты)
+//      b.appendChild(a.childNodes[1]);
+//    }
+//    a.style.height = b.getBoundingClientRect().height + 'px';  // если под скользящим элементом есть другие блоки, можно своё значение
+//    a.style.padding = '0';
+//    a.style.border = '0';  // если элементу присвоен padding или border
+//  }
+//  if (a.getBoundingClientRect().top < 15) { // elem.getBoundingClientRect() возвращает в px координаты элемента относительно верхнего левого угла области просмотра окна браузера
+//    b.className = 'sticky';
+//  } else {
+//    b.className = '';
+//  }
+//  window.addEventListener('resize', function() {
+//    a.children[0].style.width = getComputedStyle(a, '').width
+//  }, false);  // если изменить размер окна браузера, измениться ширина элемента
+//}
+//})()
